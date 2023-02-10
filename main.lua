a087263b16ecd815872b64328cb2c29d3d79f474
TheBestSalmonX <best.web.gq@gmail.com>
2023-02-10 19:09:30 +0700

    update commit

--- main.lua
+++ main.lua
@@ -1,6 +1,43 @@
 local settings = {
-    Team = "Marines" --Marines, Pirates
+    Team = "Marines", --[Marines, Pirates] ChooseTeam Default.
+    AlwaysOnline = true --Anti AFK Function.
 }
+
+function LoadSettings()
+    if readfile and writefile and isfile and isfolder then
+        if not isfolder("RulesX Hub Premium Scripts") then
+            makefolder("RulesX Hub Premium Scripts")
+        end
+        if not isfolder("RulesX Hub Premium Scripts/Blox Fruits/") then
+            makefolder("RulesX Hub Premium Scripts/Blox Fruits/")
+        end
+        if not isfile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json") then
+            writefile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(settings.SaveSetting))\
+        else
+            local Decode = game:GetService("HttpService"):JSONDecode(readfile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"))
+            for i,v in pairs(Decode) do
+                settings[i] = v
+            end
+        end
+    end
+end
+
+function SaveSettings()
+    if readfile and writefile and isfile and isfolder then
+        if not isfile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json") then
+            LoadSettings()
+        else
+            local Decode = game:GetService("HttpService"):JSONDecode(readfile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"))
+            local Array = {}
+            for i,v in pairs(settings) do
+                Array[i] = v
+            end
+            writefile("RulesX Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(Array))
+        end
+    else
+        return warn("Status : Undetected Executor")
+    end
+end
 
 if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
     repeat wait()
